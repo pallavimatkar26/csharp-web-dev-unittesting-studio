a1a2d4409836311e19e79f089efd6301feb5b0d9
pallavi <pallavimatkar26@gmail.com>
2021-12-01 22:52:09 -0600

    unit testing code commited

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -13,5 +13,55 @@ namespace BalancedBracketsTests
         {
             Assert.AreEqual(true, true);
         }
+        [TestMethod]
+        public void OnlyBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
+        [TestMethod]
+        public void BlankStringReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(""));
+        }
+        [TestMethod]
+        public void StringBracketsInsideReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[Banana Cat]"));
+        }
+        [TestMethod]
+        public void BracketsAfterWordReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("Banana [Cat]"));
+        }
+        [TestMethod]
+        public void BracketsBeforeWordReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]Banana Cat"));
+        }
+        [TestMethod]
+        public void SingleBracketReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+        }
+        [TestMethod]
+        public void SingleBracketWithWordReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[Banana Cat"));
+        }
+        [TestMethod]
+        public void UnblacedBracketReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]["));
+        }
+        [TestMethod]
+        public void OnlyOpenBracketReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+        }
+        [TestMethod]
+        public void OnlyCloseBracketReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]"));
+        }
     }
 }
